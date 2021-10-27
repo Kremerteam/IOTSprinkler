d8ecf66a32d022e3c47d98e0840f8e53ac69e463
Alex Kremer <47732635+Kremerteam@users.noreply.github.com>
2021-10-27 13:40:33 -0500

    Create Switch.c

---
+++ Switch.c
@@ -0,0 +1,10 @@
+/*
+ LAB8
+ File Name: Switch.c
+ Author(s): Alex Kremer, Zane Zwanenburg, 
+ Initial Creation Date: 10/27/2021
+ Description: Switch Driver for reset
+ Lab Number: 8
+ TA: Hassan Iqbal
+ Date of last revision: 10/27/2021
+ */
