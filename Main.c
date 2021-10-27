6ac3db894f7bda1e7b1f5d12482ad0b3daf76928
Alex Kremer <47732635+Kremerteam@users.noreply.github.com>
2021-10-27 13:41:47 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -3,7 +3,7 @@
  File Name: Main.c
  Author(s): Alex Kremer, Zane Zwanenburg, Rithvik Dyava, Nikhil Krish
  Initial Creation Date: 10/27/2021
- Description: Switch Driver for reset
+ Description: Main Driver for Sprinkler Project
  Lab Number: 8
  TA: Hassan Iqbal
  Date of last revision: 10/27/2021
