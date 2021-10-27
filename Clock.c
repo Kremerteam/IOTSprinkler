6ae73de044f0cf127e01f251d071938e87e05f83
Alex Kremer <47732635+Kremerteam@users.noreply.github.com>
2021-10-27 13:49:34 -0500

    Create Clock.c

---
+++ Clock.c
@@ -0,0 +1,10 @@
+/*
+ LAB8
+ File Name: Clock.c
+ Author(s): Alex Kremer, Zane Zwanenburg, Rithvik Dyava, Nikhil Krish
+ Initial Creation Date: 10/27/2021
+ Description: Driver for the clock used by system
+ Lab Number: 8
+ TA: Hassan Iqbal
+ Date of last revision: 10/27/2021
+ */
