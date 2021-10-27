0768318ccbc0597c210c319c05a0799d30ec80a0
Alex Kremer <47732635+Kremerteam@users.noreply.github.com>
2021-10-27 13:41:24 -0500

    Update and rename main.c to Main.c

---
+++ Main.c
@@ -0,0 +1,10 @@
+/*
+ LAB8
+ File Name: Main.c
+ Author(s): Alex Kremer, Zane Zwanenburg, Rithvik Dyava, Nikhil Krish
+ Initial Creation Date: 10/27/2021
+ Description: Switch Driver for reset
+ Lab Number: 8
+ TA: Hassan Iqbal
+ Date of last revision: 10/27/2021
+ */
