e20f72df7dc4c3670f680200bd352fe379dcb133
Alex Kremer <47732635+Kremerteam@users.noreply.github.com>
2021-10-27 13:46:43 -0500

    Create Sprinkler.c

---
+++ Sprinkler.c
@@ -0,0 +1,10 @@
+/*
+ LAB8
+ File Name: Sprinkler.c
+ Author(s): Alex Kremer, Zane Zwanenburg, Rithvik Dyava, Nikhil Krish
+ Initial Creation Date: 10/27/2021
+ Description: Sprinkler Driver
+ Lab Number: 8
+ TA: Hassan Iqbal
+ Date of last revision: 10/27/2021
+ */
