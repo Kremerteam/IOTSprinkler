6d8cd09a550a3ecd4c342b1b93b5919b9529042b
Alex Kremer <47732635+Kremerteam@users.noreply.github.com>
2021-10-27 13:53:11 -0500

    Create Light_Sensor.c

---
+++ Light_Sensor.c
@@ -0,0 +1,10 @@
+/*
+ LAB8
+ File Name: Light_Sensor.c
+ Author(s): Alex Kremer, Zane Zwanenburg, Rithvik Dyava, Nikhil Krish
+ Initial Creation Date: 10/27/2021
+ Description: Driver for light detection
+ Lab Number: 8
+ TA: Hassan Iqbal
+ Date of last revision: 10/27/2021
+ */
