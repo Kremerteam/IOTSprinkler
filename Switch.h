10b2015e5aeb95a2a21b70e42157efa0fd85e3be
Zane A Zwanenburg <zanezwan@gmail.com>
2021-11-01 13:56:48 -0500

    Update Switch.h

--- Switch.h
+++ Switch.h
@@ -1,3 +1,18 @@
+/*
+ LAB8
+ File Name: Switch.h
+ Author(s): Alex Kremer, Zane Zwanenburg, 
+ Initial Creation Date: 10/27/2021
+ Description: Switch Driver for reset
+ Lab Number: 8
+ TA: Hassan Iqbal
+ Date of last revision: 10/27/2021
+ */
 
 void DelayWait10ms(uint32_t n){};
 
+void Switch_On(void){};
+
+void Switch_Off(void){};
+
+void Switches_Init(void){};
